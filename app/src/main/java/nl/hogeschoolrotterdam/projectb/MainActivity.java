ef41cebe07b07516f9bb254262b1b96a1c7e58e4
MrTheGood <maarten@degoedeweb.nl>
2019-03-05 15:20:52 +0100

    Remove Kotlin (Kotlin is not allowed)

---
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/MainActivity.java
@@ -0,0 +1,19 @@
+package nl.hogeschoolrotterdam.projectb;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by maartendegoede on 05/03/2019.
+ * Copyright © 2019 insertCode.eu. All rights reserved.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
