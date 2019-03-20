30f2a6f202b942c6ce8a20eb546b39f81f7bab93
MrTheGood <maarten@degoedeweb.nl>
2019-03-20 11:17:17 +0100

    Remove Adil from copyright

--- .idea/fileTemplates/includes/File Header.java
+++ .idea/fileTemplates/includes/File Header.java
@@ -1,4 +1,4 @@
 /**
  * Created by ${USER} on ${DATE}.
- * Copyright © ${YEAR} Adil Yazor, Anass El Mahdaoui, Hicham El Marzgioui, Michaël van Asperen, Wesley de Man, Maarten de Goede all rights reserved.
+ * Copyright © ${YEAR} Anass El Mahdaoui, Hicham El Marzgioui, Michaël van Asperen, Wesley de Man, Maarten de Goede all rights reserved.
  */
