efdeb9f0de44c906f5bf0dc513207fb956db905a
MrTheGood <maarten@degoedeweb.nl>
2019-03-20 16:50:52 +0100

    Show random memory every time MemoriesFragment is shown as an example

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/MemoriesFragment.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/MemoriesFragment.java
@@ -9,6 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import nl.hogeschoolrotterdam.projectb.R;
+import nl.hogeschoolrotterdam.projectb.data.Database;
+import nl.hogeschoolrotterdam.projectb.data.Memory;
+
+import java.util.List;
 
 /**
  * Created by maartendegoede on 20/03/2019.
@@ -22,5 +26,22 @@ public class MemoriesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_memories, container, false);
         ((TextView) view.findViewById(R.id.textview)).setText(R.string.menu_memories);
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getView() == null) return;
+
+        // get list of memories
+        List<Memory> memories = Database.getInstance().getMemories();
+
+        // get random memory
+        Memory memory = memories.get((int) (Math.random() * memories.size()));
+
+        // set memory texts
+        ((TextView) getView().findViewById(R.id.memory_date)).setText(memory.getDateText());
+        ((TextView) getView().findViewById(R.id.memory_title)).setText(memory.getTitle());
+        ((TextView) getView().findViewById(R.id.memory_description)).setText(memory.getDescription());
     }
 }
