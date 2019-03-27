cd9579042c78312445664b9a33f83b79b846aae8
Anass <Anass_M06@hotmail.com>
2019-03-27 12:01:58 +0100

    update gradle

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/Memories_Adapter.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/Memories_Adapter.java
@@ -1,5 +1,6 @@
 package nl.hogeschoolrotterdam.projectb.fragment;
 
+import android.content.Intent;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,19 +10,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.core.widget.ListViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
+import nl.hogeschoolrotterdam.projectb.MemoryDetailActivity;
 import nl.hogeschoolrotterdam.projectb.R;
 import nl.hogeschoolrotterdam.projectb.data.Database;
 import nl.hogeschoolrotterdam.projectb.data.Memory;
 
 import java.util.List;
 
-public class Memories_Adapter extends RecyclerView.Adapter<Memories_Adapter.Memories_Viewholder> {
 
+public class Memories_Adapter extends RecyclerView.Adapter<Memories_Adapter.Memories_Viewholder> {
 
     private List<Memory> data;
 
-    public Memories_Adapter(List<Memory>data) {
-         this.data = data;
+    public Memories_Adapter(List<Memory> data) {
+        this.data = data;
 
     }
 
@@ -36,10 +38,17 @@ public class Memories_Adapter extends RecyclerView.Adapter<Memories_Adapter.Memo
 
     @Override
     public void onBindViewHolder(@NonNull Memories_Viewholder holder, int position) {
-        Memory memory = data.get(position);
+        final Memory memory = data.get(position);
         holder.Textviewtitle.setText(memory.getTitle());
         holder.Textviewdate.setText(memory.getDateText());
-
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MemoryDetailActivity.class);
+                intent.putExtra("EXTRA_SESSION_ID", memory.getId());
+                v.getContext().startActivity(intent);
+            }
+        });
 
     }
 
@@ -49,12 +58,14 @@ public class Memories_Adapter extends RecyclerView.Adapter<Memories_Adapter.Memo
     }
 
     public class Memories_Viewholder extends RecyclerView.ViewHolder {
+        View itemView;
         ImageView imageView;
         TextView Textviewtitle;
         TextView Textviewdate;
 
         public Memories_Viewholder(@NonNull View itemView) {
             super(itemView);
+            this.itemView = itemView;
             imageView = itemView.findViewById(R.id.imageView);
             Textviewtitle = itemView.findViewById(R.id.Textviewtitle);
             Textviewdate = itemView.findViewById(R.id.Textviewdate);
