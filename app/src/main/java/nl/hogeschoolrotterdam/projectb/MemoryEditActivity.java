56f00445f475fda969683c2cb3403a4da8accc52
MrTheGood <maarten@degoedeweb.nl>
2019-03-27 22:19:08 +0100

    Add input validation for memory title & description

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/MemoryEditActivity.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/MemoryEditActivity.java
@@ -2,24 +2,29 @@ package nl.hogeschoolrotterdam.projectb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import nl.hogeschoolrotterdam.projectb.data.Database;
 import nl.hogeschoolrotterdam.projectb.data.Memory;
 import nl.hogeschoolrotterdam.projectb.data.media.Media;
+import nl.hogeschoolrotterdam.projectb.util.SimpleTextWatcher;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 public class MemoryEditActivity extends AppCompatActivity {
-    TextInputEditText titleInput;
-    TextInputEditText dateInput;
-    TextInputEditText descriptionInput;
+    TextInputLayout titleInput;
+    TextInputLayout dateInput;
+    TextInputLayout descriptionInput;
     Button saveButton;
+
+    Boolean isTitleValid = false;
+    Boolean isDescriptionValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +37,37 @@ public class MemoryEditActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
 
-        titleInput = findViewById(R.id.memory_add_title_input);
-        dateInput = findViewById(R.id.memory_add_date_input);
-        descriptionInput = findViewById(R.id.memory_add_description_input);
+        titleInput = findViewById(R.id.memory_add_title);
+        dateInput = findViewById(R.id.memory_add_date);
+        descriptionInput = findViewById(R.id.memory_add_description);
         saveButton = findViewById(R.id.memory_save_button);
+
+        setButtonEnabled();
+        titleInput.getEditText().addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                isTitleValid = s.toString().length() > 1;
+                titleInput.setError(isTitleValid ? null : getString(R.string.error_title_too_short));
+                setButtonEnabled();
+            }
+        });
+        descriptionInput.getEditText().addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                isDescriptionValid = s.toString().length() > 1;
+                descriptionInput.setError(isDescriptionValid ? null : getString(R.string.error_description_too_short));
+                setButtonEnabled();
+            }
+        });
+
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LatLng location = new LatLng(0, 0);
                 Date date = new Date();
-                String title = titleInput.getText().toString();
-                String description = descriptionInput.getText().toString();
+                String title = titleInput.getEditText().getText().toString();
+                String description = descriptionInput.getEditText().getText().toString();
                 ArrayList<Media> media = null;
 
                 Memory memory = new Memory(
@@ -63,6 +87,10 @@ public class MemoryEditActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void setButtonEnabled() {
+        saveButton.setEnabled(isDescriptionValid && isTitleValid);
     }
 
     @Override
