d1e03ff5437cfc7304ebe5f3cbd64f4e1b52a6d1
Wesley <keesdelol@hotmail.com>
2019-03-27 12:07:30 +0100

    Mapview with some pointers and with a clickable marker 3.0

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/MapFragment.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/MapFragment.java
@@ -1,11 +1,13 @@
 package nl.hogeschoolrotterdam.projectb.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -13,11 +15,13 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import nl.hogeschoolrotterdam.projectb.MemoryDetailActivity;
 import nl.hogeschoolrotterdam.projectb.R;
 import nl.hogeschoolrotterdam.projectb.data.Database;
 import nl.hogeschoolrotterdam.projectb.data.Memory;
@@ -29,12 +33,12 @@ import nl.hogeschoolrotterdam.projectb.data.Memory;
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     GoogleMap mGoogleMap;
-    MapView  mMapView;
+    MapView mMapView;
     View mView;
 
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
@@ -42,7 +46,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mView  = inflater.inflate(R.layout.fragment_map, container, false);
+        mView = inflater.inflate(R.layout.fragment_map, container, false);
 
         // texten setten
 
@@ -54,24 +58,38 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState);
 
         mMapView = (MapView) mView.findViewById(R.id.map);
-        if (mMapView != null){
+        if (mMapView != null) {
             mMapView.onCreate(null);
             mMapView.onResume();
             mMapView.getMapAsync(this);
 
         }
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getContext());
         mGoogleMap = googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.1326,5.2913)).title("Nederland").snippet("WHIP app"));
-        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(52.1326,5.2913)).zoom(15).bearing(0).tilt(0).build();
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
-        for (Memory memorie : Database.getInstance().getMemories()){
-            googleMap.addMarker(new MarkerOptions().position(memorie.getLocation()).title(memorie.getTitle()).snippet((String) memorie.getDateText()));
+        mGoogleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
 
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String memoryId = (String) marker.getTag();
+                Intent intent = new Intent(getContext(), MemoryDetailActivity.class);
+                intent.putExtra("EXTRA_SESSION_ID", memoryId);
+                startActivity(intent);
+                return false;
+            }
+        });
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.1326, 5.2913)).title("Nederland").snippet("WHIP app"));
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(52.1326, 5.2913)).zoom(10).bearing(0).tilt(0).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+        for (Memory memorie : Database.getInstance().getMemories()) {
+            Marker marker = googleMap.addMarker(new MarkerOptions().position(memorie.getLocation()).title(memorie.getTitle()).snippet((String) memorie.getDateText()));
+            marker.setTag(memorie.getId());
         }
     }
+
+
 }
