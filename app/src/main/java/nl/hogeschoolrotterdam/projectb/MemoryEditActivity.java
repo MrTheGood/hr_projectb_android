a95da3ccdc6500523e2c222a2227c5b71a8dfeeb
MrTheGood <maarten@degoedeweb.nl>
2019-03-30 14:01:28 +0100

    Cleanup MemoryEditActivity

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/MemoryEditActivity.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/MemoryEditActivity.java
@@ -36,71 +36,81 @@ public class MemoryEditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_edit);
 
+        // initialise views
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
 
-        ArrayList<Media> media = null;
-        memory = new Memory(
-                Database.getInstance().newId(),
-                new LatLng(0, 0),
-                new Date(),
-                "",
-                "",
-                media
-        );
-
         titleInput = findViewById(R.id.memory_add_title);
         dateInput = findViewById(R.id.memory_add_date);
         descriptionInput = findViewById(R.id.memory_add_description);
         saveButton = findViewById(R.id.memory_save_button);
 
-        setButtonEnabled();
-        dateInput.getEditText().setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(final View v, boolean hasFocus) {
-                if (hasFocus) {
-                    final Calendar calendar = Calendar.getInstance();
-                    int year = calendar.get(Calendar.YEAR);
-                    int month = calendar.get(Calendar.MONTH);
-                    int day = calendar.get(Calendar.DAY_OF_MONTH);
-                    new DatePickerDialog(MemoryEditActivity.this,
-                            new DatePickerDialog.OnDateSetListener() {
-                                @Override
-                                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                                    calendar.set(year, month - 1, day, 0, 0);
-                                    memory.setDate(new Date(calendar.getTimeInMillis()));
-                                    dateInput.getEditText().setText(memory.getDateText());
-                                    findViewById(v.getNextFocusDownId()).requestFocus();
-                                }
-                            },
-                            year, month, day)
-                            .show();
-                }
-            }
-        });
+
+        // create a memory with calendar to today
+        final Calendar calendar = Calendar.getInstance();
+        ArrayList<Media> media = null;
+        memory = new Memory(
+                Database.getInstance().newId(),
+                new LatLng(0, 0),
+                new Date(calendar.getTimeInMillis()),
+                "",
+                "",
+                media
+        );
+
+
+        setButtonEnabled(); // save button should be disabled when input not yet valid
+
+        // check if the title is valid when the text has changed
         titleInput.getEditText().addTextChangedListener(new SimpleTextWatcher() {
             @Override
             public void afterTextChanged(Editable s) {
                 isTitleValid = s.toString().length() > 1;
-                titleInput.setError(isTitleValid ? null : getString(R.string.error_title_too_short));
+                titleInput.setError(isTitleValid ? null : getString(R.string.error_title_too_short)); // show an error if title is too short
                 memory.setTitle(s.toString());
                 setButtonEnabled();
             }
         });
+
+        // Open date picker if date input was selected
+        dateInput.getEditText().setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(final View v, boolean hasFocus) {
+                if (hasFocus) {
+                    new DatePickerDialog(
+                            MemoryEditActivity.this,
+                            new DatePickerDialog.OnDateSetListener() {
+                                @Override
+                                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                    calendar.set(year, month - 1, day, 0, 0); // set calendar to the selected time
+                                    memory.setDate(new Date(calendar.getTimeInMillis())); // update memory date
+                                    dateInput.getEditText().setText(memory.getDateText()); // set input text
+                                    findViewById(v.getNextFocusDownId()).requestFocus(); // request focus for the next input
+                                }
+                            },
+                            calendar.get(Calendar.YEAR), // picker dialog selected year
+                            calendar.get(Calendar.MONTH), // picker dialog selected month
+                            calendar.get(Calendar.DAY_OF_MONTH) // picker dialog selected day
+                    ).show();
+                }
+            }
+        });
+
+        // check if the description is valid when the text has changed
         descriptionInput.getEditText().addTextChangedListener(new SimpleTextWatcher() {
             @Override
             public void afterTextChanged(Editable s) {
                 isDescriptionValid = s.toString().length() > 1;
-                descriptionInput.setError(isDescriptionValid ? null : getString(R.string.error_description_too_short));
+                descriptionInput.setError(isDescriptionValid ? null : getString(R.string.error_description_too_short)); // show an error if description is too short
                 memory.setDescription(s.toString());
                 setButtonEnabled();
             }
         });
 
-
+        // save and open new memory on save clicked
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
