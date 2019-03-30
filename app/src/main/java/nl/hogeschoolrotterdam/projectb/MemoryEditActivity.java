207227ccefd53b8bd8b3d1c2ae504b8daa53ff68
MrTheGood <maarten@degoedeweb.nl>
2019-03-30 13:54:29 +0100

    Add some DatePickerDialog magic :D

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/MemoryEditActivity.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/MemoryEditActivity.java
@@ -1,10 +1,12 @@
 package nl.hogeschoolrotterdam.projectb;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import com.google.android.gms.maps.model.LatLng;
@@ -15,16 +17,19 @@ import nl.hogeschoolrotterdam.projectb.data.media.Media;
 import nl.hogeschoolrotterdam.projectb.util.SimpleTextWatcher;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MemoryEditActivity extends AppCompatActivity {
-    TextInputLayout titleInput;
-    TextInputLayout dateInput;
-    TextInputLayout descriptionInput;
-    Button saveButton;
+    private TextInputLayout titleInput;
+    private TextInputLayout dateInput;
+    private TextInputLayout descriptionInput;
+    private Button saveButton;
 
-    Boolean isTitleValid = false;
-    Boolean isDescriptionValid = false;
+    private Boolean isTitleValid = false;
+    private Boolean isDescriptionValid = false;
+
+    private Memory memory = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +42,51 @@ public class MemoryEditActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
 
+        ArrayList<Media> media = null;
+        memory = new Memory(
+                Database.getInstance().newId(),
+                new LatLng(0, 0),
+                new Date(),
+                "",
+                "",
+                media
+        );
+
         titleInput = findViewById(R.id.memory_add_title);
         dateInput = findViewById(R.id.memory_add_date);
         descriptionInput = findViewById(R.id.memory_add_description);
         saveButton = findViewById(R.id.memory_save_button);
 
         setButtonEnabled();
+        dateInput.getEditText().setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(final View v, boolean hasFocus) {
+                if (hasFocus) {
+                    final Calendar calendar = Calendar.getInstance();
+                    int year = calendar.get(Calendar.YEAR);
+                    int month = calendar.get(Calendar.MONTH);
+                    int day = calendar.get(Calendar.DAY_OF_MONTH);
+                    new DatePickerDialog(MemoryEditActivity.this,
+                            new DatePickerDialog.OnDateSetListener() {
+                                @Override
+                                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                    calendar.set(year, month - 1, day, 0, 0);
+                                    memory.setDate(new Date(calendar.getTimeInMillis()));
+                                    dateInput.getEditText().setText(memory.getDateText());
+                                    findViewById(v.getNextFocusDownId()).requestFocus();
+                                }
+                            },
+                            year, month, day)
+                            .show();
+                }
+            }
+        });
         titleInput.getEditText().addTextChangedListener(new SimpleTextWatcher() {
             @Override
             public void afterTextChanged(Editable s) {
                 isTitleValid = s.toString().length() > 1;
                 titleInput.setError(isTitleValid ? null : getString(R.string.error_title_too_short));
+                memory.setTitle(s.toString());
                 setButtonEnabled();
             }
         });
@@ -56,6 +95,7 @@ public class MemoryEditActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 isDescriptionValid = s.toString().length() > 1;
                 descriptionInput.setError(isDescriptionValid ? null : getString(R.string.error_description_too_short));
+                memory.setDescription(s.toString());
                 setButtonEnabled();
             }
         });
@@ -64,21 +104,6 @@ public class MemoryEditActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LatLng location = new LatLng(0, 0);
-                Date date = new Date();
-                String title = titleInput.getEditText().getText().toString();
-                String description = descriptionInput.getEditText().getText().toString();
-                ArrayList<Media> media = null;
-
-                Memory memory = new Memory(
-                        Database.getInstance().newId(),
-                        location,
-                        date,
-                        title,
-                        description,
-                        media
-                );
-
                 Database.getInstance().addMemory(memory);
 
                 Intent intent = new Intent(MemoryEditActivity.this, MemoryDetailActivity.class);
