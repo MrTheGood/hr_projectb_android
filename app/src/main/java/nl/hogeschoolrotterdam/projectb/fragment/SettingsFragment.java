79f786f7df21bb7054acde71cccc5ca7c37b020b
MrTheGood <maarten@degoedeweb.nl>
2019-04-02 17:43:13 +0200

    Finish select theme styling, show preview of theme

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/SettingsFragment.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/SettingsFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +23,8 @@ public class SettingsFragment extends Fragment {
     private static final int LIGHT = 0;
     private static final int DARK = 1;
     private static final int LIGHT_PURPLE = 2;
+    private Spinner spinner;
+    private ImageView previewImage;
 
     @Nullable
     @Override
@@ -32,9 +36,24 @@ public class SettingsFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        Spinner spinner = getView().findViewById(R.id.theme_selector);
-        int themeId = WhibApp.getInstance().getThemeId();
-        switch (themeId) {
+        spinner = getView().findViewById(R.id.theme_selector);
+        previewImage = getView().findViewById(R.id.theme_preview);
+        Button applyButton = getView().findViewById(R.id.theme_apply);
+
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                applyTheme();
+            }
+        });
+        spinner.setOnItemSelectedListener(new SimpleOnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                themeSelected(position);
+            }
+        });
+
+        switch (WhibApp.getInstance().getThemeId()) {
             case R.style.AppTheme_Light:
                 spinner.setSelection(LIGHT, true);
                 break;
@@ -45,25 +64,36 @@ public class SettingsFragment extends Fragment {
                 spinner.setSelection(LIGHT_PURPLE, true);
                 break;
         }
+    }
 
-        spinner.setOnItemSelectedListener(new SimpleOnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case LIGHT: // Light theme
-                        WhibApp.getInstance().setCurrentTheme(R.style.AppTheme_Light, false);
-                        requireActivity().recreate();
-                        break;
-                    case DARK: // Dark theme
-                        WhibApp.getInstance().setCurrentTheme(R.style.AppTheme_Dark, true);
-                        requireActivity().recreate();
-                        break;
-                    case LIGHT_PURPLE: // Light Purple theme
-                        WhibApp.getInstance().setCurrentTheme(R.style.AppTheme_Light_Purple, false);
-                        requireActivity().recreate();
-                        break;
-                }
-            }
-        });
+    private void themeSelected(int theme) {
+        switch (theme) {
+            case LIGHT:
+                previewImage.setImageResource(R.drawable.img_theme_light);
+                break;
+            case DARK:
+                previewImage.setImageResource(R.drawable.img_theme_dark);
+                break;
+            case LIGHT_PURPLE:
+                previewImage.setImageResource(R.drawable.img_theme_purple);
+                break;
+        }
+    }
+
+    private void applyTheme() {
+        switch (spinner.getSelectedItemPosition()) {
+            case LIGHT:
+                WhibApp.getInstance().setCurrentTheme(R.style.AppTheme_Light, false);
+                requireActivity().recreate();
+                break;
+            case DARK:
+                WhibApp.getInstance().setCurrentTheme(R.style.AppTheme_Dark, true);
+                requireActivity().recreate();
+                break;
+            case LIGHT_PURPLE:
+                WhibApp.getInstance().setCurrentTheme(R.style.AppTheme_Light_Purple, false);
+                requireActivity().recreate();
+                break;
+        }
     }
 }
