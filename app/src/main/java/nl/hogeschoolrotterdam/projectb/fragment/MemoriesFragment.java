f8bb9e8b6ade2a849348d90e80df7500faf59676
Hicham010 <47334105+Hicham010@users.noreply.github.com>
2019-05-16 00:01:52 +0200

    Add a drawer layout to memoriesfragement

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/MemoriesFragment.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/fragment/MemoriesFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import nl.hogeschoolrotterdam.projectb.R;
+import nl.hogeschoolrotterdam.projectb.WhibApp;
 import nl.hogeschoolrotterdam.projectb.adapter.MemoriesAdapter;
 import nl.hogeschoolrotterdam.projectb.data.Database;
 import nl.hogeschoolrotterdam.projectb.data.room.entities.Memory;
@@ -26,6 +29,8 @@ import java.util.List;
 public class MemoriesFragment extends Fragment {
     private List<Memory> memories;
     private MemoriesAdapter adapter;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Nullable
     @Override
@@ -35,7 +40,6 @@ public class MemoriesFragment extends Fragment {
 
         memories = Database.getInstance().getMemories();
         adapter = new MemoriesAdapter(memories);
-
 
         RecyclerView recyclerView = view.findViewById(R.id.memorylist);
         recyclerView.setAdapter(adapter);
@@ -49,8 +53,16 @@ public class MemoriesFragment extends Fragment {
         Toolbar tb = view.findViewById(R.id.toolbar);
         ((AppCompatActivity) requireActivity()).setSupportActionBar(tb);
 
+
+
+        mDrawerLayout=(DrawerLayout) view.findViewById(R.id.drawer);
+        mToggle= new ActionBarDrawerToggle(getActivity(),mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        ((AppCompatActivity)getActivity()).getSupportActionBar();
         return view;
     }
+
 
     @Override
     public void onResume() {
@@ -67,6 +79,10 @@ public class MemoriesFragment extends Fragment {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
         switch (item.getItemId()) {
             case R.id.Newest:
                 Collections.sort(memories, new Comparator<Memory>() {
