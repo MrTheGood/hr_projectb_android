19ed027cef2bd546be09ce0b0c4b6f67bd7f5fa8
MrTheGood <maarten@degoedeweb.nl>
2019-05-22 20:37:46 +0200

    Add OnboardingActivity

---
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/ActivityOnboarding.java
@@ -0,0 +1,18 @@
+package nl.hogeschoolrotterdam.projectb;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by maartendegoede on 2019-05-22.
+ * Copyright © 2019 insertCode.eu. All rights reserved.
+ */
+public class ActivityOnboarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
