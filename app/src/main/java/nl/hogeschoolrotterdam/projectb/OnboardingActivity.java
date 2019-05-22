efbdd22fdcde45b18a6e8f03e6a053fd092cc406
MrTheGood <maarten@degoedeweb.nl>
2019-05-22 22:44:33 +0200

    Make themes work in onboarding activity

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/OnboardingActivity.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/OnboardingActivity.java
@@ -22,6 +22,7 @@ public class OnboardingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(WhibApp.getInstance().getThemeId());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
