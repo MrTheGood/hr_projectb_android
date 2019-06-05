032dce34bdb5d817254122147e545ddaf5b31ec2
MrTheGood <maarten@degoedeweb.nl>
2019-06-05 16:28:02 +0200

    Set current password to change password

--- app/src/main/java/nl/hogeschoolrotterdam/projectb/SetPasswordActivity.java
+++ app/src/main/java/nl/hogeschoolrotterdam/projectb/SetPasswordActivity.java
@@ -15,14 +15,25 @@ public class SetPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_password);
 
+        final EditText oldPassword = findViewById(R.id.old_password_input);
         final EditText password = findViewById(R.id.set_password_input);
         final EditText repeatPassword = findViewById(R.id.set_password_repeat_input);
         final CheckBox useFingerprint = findViewById(R.id.use_fingerprint);
         useFingerprint.setVisibility(Build.VERSION.SDK_INT >= 28 ? View.VISIBLE : View.GONE);
+        oldPassword.setVisibility(WhibApp.getInstance().hasPassword() ? View.VISIBLE : View.GONE);
 
         findViewById(R.id.set_password_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                password.setError(null);
+                repeatPassword.setError(null);
+                oldPassword.setError(null);
+
+                if (WhibApp.getInstance().hasPassword() && WhibApp.getInstance().checkPassword(oldPassword.getText().toString())) {
+                    oldPassword.setError(getString(R.string.error_invalid_password));
+                    return;
+                }
+
                 String p = password.getText().toString();
                 String rP = repeatPassword.getText().toString();
                 if (p.isEmpty()) {
